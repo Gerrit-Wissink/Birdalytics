fceae4e7a621d083a2ea3e749f6b1e3642ce7e9c
David Kim <dyk8585@rit.edu>
2026-01-29 17:55:27 -0500

    Refactored motion_sensor.py to be more efficient; added placeholders for time elapsed function. Currently not working.

--- hardware/motion_sensor.py
+++ hardware/motion_sensor.py
@@ -15,16 +15,27 @@ bird_cam.start()
 
 motion_sensor = GPIO(17)
 
-while True:
-    if motion_sensor.is_active:
+def capture_photo(condition):
         capture_time = datetime.datetime.now()
         timestamp = capture_time.strftime("%Y-%m-%d_%H-%M-%S")
         filename = "birdbox_" + timestamp
         path = {home_dir} + "/Photos/" + filename + ".jpg" # TODO: fix this. it's the only thing not working right now
-        print("Motion detected. Capturing photo...")
-        bird_cam.start_and_capture_file(path)
+        if condition == 1:
+            print("Motion detected.")
+        elif condition == 2:
+            print("Time elapsed.")
+        else:
+            raise ValueError("Fatal error! Unknown condition " + condition)
+        print("Capturing photo...")
+        bird_cam.capture_file(path)
         print("Photo saved to " + path)
         sleep(15)
+
+while True:
+    if motion_sensor.is_active:
+        capture_photo(1)
+    elif timepassed:
+        capture_photo(2)
     else:
         print("Waiting for motion...")
         sleep(1)
