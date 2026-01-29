10201e0fadcd72466e941291a4102713ac194115
David Kim <dyk8585@rit.edu>
2026-01-29 17:38:09 -0500

    Created LAN.py to test wireless access point capabilities. Currently untested: will create pull request when script is confirmed to be working.

---
+++ hardware/LAN.py
@@ -0,0 +1,17 @@
+from gpiozero import DigitalInputDevice as GPIO
+from time import sleep
+import os
+
+# TODO: find a pin to wire the LAN enable switch to
+push_button = GPIO(0)
+
+# ensure Wi-Fi isn't running
+os.system("rfkill block wifi")
+
+while True:
+    if push_button.is_active:
+        print("Switch detected. Enabling LAN...")
+        os.system("rfkill unblock wifi")
+        sleep(15) # anywhere between 30-60 mins? will need testing
+        os.system("rfkill block wifi")
+        sleep(5)
