75d1b5ce8ab3f6fe5c247bad997f1c91709c87e4
Elise <emh5943@rit.edu>
2026-02-22 21:48:55 -0500

    overview page complete

--- frontend/website/src/pages/cameraInfo.jsx
+++ frontend/website/src/pages/cameraInfo.jsx
@@ -1,5 +1,20 @@
 export default function CamInfo(){
     return(
-        <h1>Camera Info Page</h1>
+        <section id='camInfo-container'>
+            <div id='camera-sidebar'>
+                <h2>Cameras</h2>
+            </div>
+            <div id='camera-content'>
+                <h1>Camera Name</h1>
+                <div className = 'side-by-side'>
+                    <div id='camera-summary'>
+                        <h3>Camera Summary</h3>
+                    </div>
+                    <div id='identify-box'>
+                        <h3>Species Identification</h3>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
