8674493a0ac8f35dfa369f16e2b91bd2747c8c42
Anthony Garcia <ajg3062@rit.edu>
2026-03-31 17:48:28 -0400

    readd vite config to repo

--- frontend/website/vite.config.js
+++ frontend/website/vite.config.js
@@ -1,11 +1,8 @@
-/* eslint-env node */
-
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  const isDocker = env.VITE_DOCKER === "true";
+export default defineConfig(() => {
+  const isDocker = import.meta.env.VITE_DOCKER === "true";
 
   return {
     plugins: [react()],
